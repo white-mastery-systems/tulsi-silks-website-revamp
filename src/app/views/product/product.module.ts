dcb13285059087e0fa79143a4a146489e616ec7a
shankarijeesha <shankarimuniyasamy@gmail.com>
2025-05-22 12:25:33 +0530

    design correction

--- src/app/views/product/product.module.ts
+++ src/app/views/product/product.module.ts
@@ -8,7 +8,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { CustomizationModule } from '../../shared/modules/customization/customization.module';
 import { ProductComponent } from './product.component';
 import { RelatedProductsDirective } from './directives/related-products.directive';
-import { LucideAngularModule, ChevronLeft, ChevronRight } from 'lucide-angular';
+import { LucideAngularModule, ChevronLeft, ChevronRight, XCircle } from 'lucide-angular';
 
 @NgModule({
   declarations: [ProductComponent, RelatedProductsDirective],
@@ -19,7 +19,7 @@ import { LucideAngularModule, ChevronLeft, ChevronRight } from 'lucide-angular';
     YouTubePlayerModule,
     NgxPaginationModule,
     AccordionModule.forRoot(),
-    LucideAngularModule.pick({ ChevronLeft, ChevronRight })
+    LucideAngularModule.pick({ ChevronLeft, ChevronRight, XCircle })
   ]
 })
 
