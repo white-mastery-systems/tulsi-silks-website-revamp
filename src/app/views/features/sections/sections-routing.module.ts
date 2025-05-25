67af46f4254d9e5a0779a226745cae940b333d69
Ashwant R <ashok9730@gmail.com>
2025-05-25 19:01:39 +0530

    section-page

---
+++ src/app/views/features/sections/sections-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SectionsComponent } from './sections.component';
+
+const routes: Routes = [{ path: '', component: SectionsComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class SectionsRoutingModule { }
