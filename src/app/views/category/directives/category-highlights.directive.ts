78d33b6ca8f9a1a1d9892a315973004f52b1ed42
sathya1721 <52814078+sathya1721@users.noreply.github.com>
2025-09-01 15:13:05 +0530

    refactor(category): enhance layout and responsiveness of category components

--- src/app/views/category/directives/category-highlights.directive.ts
+++ src/app/views/category/directives/category-highlights.directive.ts
@@ -16,9 +16,9 @@ export class CategoryHighlightsDirective {
   color_swiper: any ={
     auto_play: false,
     break_points: {
-      1024: { slidesPerView: 7, spaceBetween: 0 },
-      768: { slidesPerView: 3, spaceBetween: 0 },
-      640: { slidesPerView: 3, spaceBetween: 0 },
+      1024: { slidesPerView: 7, spaceBetween: 15 },
+      768: { slidesPerView: 3, spaceBetween: 15 },
+      640: { slidesPerView: 3, spaceBetween: 15 },
       320: { slidesPerView: 3.5, spaceBetween: 2 }
     }
   };
@@ -28,10 +28,10 @@ export class CategoryHighlightsDirective {
     auto_play: true,
     loop:true,
     break_points: {
-      1024: { slidesPerView: 5.5, spaceBetween: 0 },
-      768: { slidesPerView: 4.5, spaceBetween: 0 },
-      640: { slidesPerView: 3.5, spaceBetween: 0 },
-      320: { slidesPerView: 3.5, spaceBetween: 0 }
+      1024: { slidesPerView: 5.5, spaceBetween: 15 },
+      768: { slidesPerView: 4.5, spaceBetween: 15 },
+      640: { slidesPerView: 3.5, spaceBetween: 15 },
+      320: { slidesPerView: 3.5, spaceBetween: 15 }
     }
   };
 
